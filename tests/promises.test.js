6c430edd08ac48a04bdb35a3d1a2896c001cebb6
santi100a <santyrojasprieto9@gmail.com>
2023-03-05 20:03:16 -0500

    feat: version 1.0.6.

--- tests/promises.test.js
+++ tests/promises.test.js
@@ -1,22 +1,22 @@
 const PROMISES = require('../cjs/promises.js');
 const { delay, AsyncTimer } = PROMISES;
+const ERROR_MS = 10;
+const DELAY = 500;
 
-test('the async delay function must wait around 1/2 seconds (±10ms)', async () => {
-    const a = Date.now();
-    await delay(500);
-    const b = Date.now();
-    const c = b - a;
-   expect(c <= 510 || c >= 490)
-        .toBe(true);
+describe('promises module test suite', () => {
+    test('the delay and async timer must be valid', () => {
+        expect(typeof delay)
+            .toBe('function');
+        expect(typeof AsyncTimer)
+            .toBe('function');
+    });
+    test('the async delay function must wait around 1/2 seconds (±10ms)', async () => {
+        const a = Date.now();
+        await delay(DELAY);
+        const b = Date.now();
+        const c = b - a;
+       expect(c <= DELAY + ERROR_MS || c >= DELAY - ERROR_MS)
+            .toBe(true);
+    });
+
 });
-test('the async timer must measure no time between calls', async() => {
-    for (let i = 0; i < 20; i++) {
-        const timer = new AsyncTimer();
-        const timer2 = await timer.start();
-        const timer3 = await timer2.stop();
-        const diff = await timer3.getDifference();
-
-        expect(diff)
-        .toBe(0); 
-    }
-}); 
